007acc5417ce459209252a159d9ea54bce196b01
Alexander <javaprofi@gmx.de>
2024-10-15 12:15:09 +0200

    count word commit!

---
+++ textanalyzer/Utility/StringVerifier.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.RegularExpressions;
+
+namespace TextAnalyzer.Utility
+{
+    public class StringVerifier
+    {
+        public int count_words_in_string(string input)
+        {
+
+            int result_of_counted_words = ident_words(input).Length;
+            if (result_of_counted_words == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return result_of_counted_words;
+            }
+        }
+
+        private string[] ident_words(string input)
+        {
+
+            if (!string.IsNullOrEmpty(input))
+            {
+                string pattern = @"(?<!^)(?=[A-Z])|[ ,;.!?'\t\n]+";
+                var words = Regex.Split(input, pattern)
+                    .Where(word => !string.IsNullOrWhiteSpace(word))
+                    .Select(word => word.Trim())
+                    .ToArray();
+                return words;
+            }
+            else
+            {
+                string[] words = new string[0];
+                return words;
+            }
+
+        }
+    }
+}
---
+++ textanalyzertests/utilitytests/StringVerifierTest.cs
@@ -0,0 +1,81 @@
+﻿using NUnit.Framework.Legacy;
+using TextAnalyzer.Utility;
+
+namespace TextAnalyzer.utilitytests
+{
+    public class StringVerifierTest
+    {
+        private StringVerifier strVerfier;
+
+        [SetUp]
+        public void Setup()
+        {
+            strVerfier = new StringVerifier();
+        }
+
+        [Test]
+        public void checkIfStringVerfierCanBeCreatd()
+        {
+            ClassicAssert.NotNull(strVerfier);
+        }
+
+        [Test]
+        public void checkCountOneWordInGivenString()
+        {
+            int countedWords = 1;
+            String givenInput = "test";
+            ClassicAssert.AreEqual(countedWords, strVerfier.count_words_in_string(givenInput));
+        }
+
+
+        [Test]
+        public void checkCountManyWordsInGivenString()
+        {
+            int countedWords = 5;
+            String givenInput = "testALikeManyWords";
+            ClassicAssert.AreEqual(countedWords, strVerfier.count_words_in_string(givenInput));
+        }
+
+        [Test]
+        public void checkCountManyWordsSeparatedByComaInAString()
+        {
+            int countedWords = 5;
+            String givenInput = "test,A,Like,Many,Words";
+            ClassicAssert.AreEqual(countedWords, strVerfier.count_words_in_string(givenInput));
+        }
+
+        [Test]
+        public void checkCountManyWordsSeparatedBySemicolonInAString()
+        {
+            int countedWords = 5;
+            String givenInput = "test;A;Like;Many;Words";
+            ClassicAssert.AreEqual(countedWords, strVerfier.count_words_in_string(givenInput));
+        }
+
+        [Test]
+        public void checkCountManyWordsSeparatedByQuestionMarkInAString()
+        {
+            int countedWords = 5;
+            String givenInput = "test?A?Like?Many?Words";
+            ClassicAssert.AreEqual(countedWords, strVerfier.count_words_in_string(givenInput));
+        }
+
+        [Test]
+        public void checkCountManyWordsSeparatedByExclamationMarkInAString()
+        {
+            int countedWords = 5;
+            String givenInput = "test!A!Like!Many!Words";
+            ClassicAssert.AreEqual(countedWords, strVerfier.count_words_in_string(givenInput));
+        }
+
+
+        [Test]
+        public void checkCountManyWordsSeparatedByDifferentSignsInAString()
+        {
+            int countedWords = 6;
+            String givenInput = "test'A;Like!Many?Words Test2";
+            ClassicAssert.AreEqual(countedWords, strVerfier.count_words_in_string(givenInput));
+        }
+
+    }
+}
