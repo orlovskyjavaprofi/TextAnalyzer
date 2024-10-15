0ca21b98df5ce658811c550cf04fafd0923593dd
Alexander <javaprofi@gmx.de>
2024-10-15 13:15:01 +0200

    implemented feature check for word appearances

---
+++ textanalyzer/Utility/StringAppearances.cs
@@ -0,0 +1,56 @@
+﻿using System.Text.RegularExpressions;
+
+namespace TextAnalyzer.Utility
+{
+    public class StringAppearances
+    {
+        public Boolean verify_word(String inputWord, String inputSentence)
+        {
+            Boolean result = false;
+
+            if (string.IsNullOrWhiteSpace(inputWord) || string.IsNullOrWhiteSpace(inputSentence))
+            {
+                return result;
+            }
+            else
+            {
+                string pattern = $@"{Regex.Escape(inputWord)}";
+                result = Regex.IsMatch(inputSentence, pattern, RegexOptions.IgnoreCase);
+            }
+
+            return result;
+        }
+
+        public Boolean verify_words(String inputWords, String inputSentence)
+        {
+            Boolean result = false;
+
+            if (string.IsNullOrWhiteSpace(inputWords) || string.IsNullOrWhiteSpace(inputSentence))
+            {
+                return false;
+            }
+            else
+            {
+                var wordsToVerify = inputWords
+                    .Split(',')
+                    .Select(word => word.Trim())
+                    .ToArray();
+
+                foreach (var word in wordsToVerify)
+                {
+                    if (!verify_word(word, inputSentence))
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        result = true;
+                    }
+                }
+                return result;
+            }
+
+        }
+
+    }
+}
---
+++ textanalyzertests/utilitytests/StringAppearancesTest.cs
@@ -0,0 +1,104 @@
+﻿using NUnit.Framework.Legacy;
+using TextAnalyzer.Utility;
+
+namespace TextAnalyzer.utilitytests
+{
+    public class StringAppearancesTest
+    {
+        private StringAppearances strAppearances;
+
+        [SetUp]
+        public void Setup()
+        {
+            strAppearances = new StringAppearances();
+        }
+
+
+        [Test]
+        public void checkIfStringAppearancesCanBeCreatd()
+        {
+            ClassicAssert.NotNull(strAppearances);
+        }
+
+        [Test]
+        public void checkIfOneWordNotGivenInGivenSentence()
+        {
+            String testWord = "";
+            String testSentence = "OneWouldSayTheUniverseConsistALotOfTheMilkyWay";
+
+            ClassicAssert.False(strAppearances.verify_word(testWord, testSentence));
+        }
+
+        [Test]
+        public void checkIfOneWordAppearsInGivenWord()
+        {
+            String testWord = "TheMilkyWay";
+            String testSentence = "OneWouldSayTheUniverseConsistALotOfTheMilkyWay";
+
+            ClassicAssert.True(strAppearances.verify_word(testWord, testSentence));
+        }
+
+        [Test]
+        public void checkIfOneWordAppearsInMiddleGivenSentence()
+        {
+            String testWord = "TheMilkyWay";
+            String testSentence = "OneWouldSayTheUniverseTheMilkyWayConsistALotOf";
+
+            ClassicAssert.True(strAppearances.verify_word(testWord, testSentence));
+        }
+
+        [Test]
+        public void checkIfOneWordAppearsInBeginOfGivenSentence()
+        {
+            String testWord = "TheMilkyWay";
+            String testSentence = "TheMilkyWayOneWouldSayTheUniverseConsistALotOf";
+
+            ClassicAssert.True(strAppearances.verify_word(testWord, testSentence));
+        }
+
+        [Test]
+        public void checkIfOneWordAppearsInSentenceSeparatedBySpaces()
+        {
+            String testWord = "Way";
+            String testSentence = "The Milky Way One Would Say The Universe Consist A Lot Of";
+
+            ClassicAssert.True(strAppearances.verify_word(testWord, testSentence));
+        }
+
+        [Test]
+        public void checkIfOneWordAppearsInSentenceSeparatedByCommaDelimeters()
+        {
+            String testWord = "Way";
+            String testSentence = "The,Milky,Way,One,Would,Say,The,Universe,Consist,A,Lot,Of";
+
+            ClassicAssert.True(strAppearances.verify_word(testWord, testSentence));
+        }
+
+        [Test]
+        public void checkIfManyWordsAppearingInTheSentenceSeparatedByCommaDelimeters()
+        {
+            String testWords = "Way,Universe";
+            String testSentence = "The,Milky,Way,One,Would,Say,The,Universe,Consist,A,Lot,Of";
+
+            ClassicAssert.True(strAppearances.verify_words(testWords, testSentence));
+        }
+
+        [Test]
+        public void checkIfManyWordsAppearingInTheSentenceSeparatedBySpaces()
+        {
+            String testWords = "Say,Of,Consist";
+            String testSentence = "Would Say The Universe Consist A Lot Of";
+
+            ClassicAssert.True(strAppearances.verify_words(testWords, testSentence));
+        }
+
+        [Test]
+        public void checkIfManyWordsAppearingInTheSentenceSeparatedByQuestionDelimiter()
+        {
+            String testWords = "congress,hearing,faces";
+            String testSentence = "New?elected?president?faces?congress?hearing";
+
+            ClassicAssert.True(strAppearances.verify_words(testWords, testSentence));
+        }
+    }
+}
