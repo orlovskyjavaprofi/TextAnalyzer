05cb3c01adcd630c632ec8da100844ad79faa8c4
Alexander <javaprofi@gmx.de>
2024-10-16 10:54:52 +0200

    added description how to use webservice

--- textanalyzer/Program.cs
+++ textanalyzer/Program.cs
@@ -1,6 +1,13 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using TextAnalyzer.Utility;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSingleton<StringVerifier>();
+builder.Services.AddControllers();
 var app = builder.Build();
 
 app.MapGet("/", () => "Text analyzer by (C)Alexander Orlovsky\nMIT license");
+app.MapControllers();
 
 app.Run();
